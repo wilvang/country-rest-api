10c5b8e6b0b8e27edd48e48952eb4734f2325aa9
Johan Fredrik Wilvang <johan@wilvang.com>
2025-02-12 13:55:18 +0100

    Created a config folder containing constants for the application. The constants.go file includes paths to the different endpoints.

---
+++ config/constants.go
@@ -0,0 +1,9 @@
+package config
+
+const (
+	// Endpoints for the service.
+	DEFAULT_PATH    = "/"
+	INFO_PATH       = "/countryinfo/v1/info/"
+	POPULATION_PATH = "/countryinfo/v1/population/"
+	STATUS_PATH     = "/countryinfo/v1/status/"
+)
