864b5b78c7bb6328fdde7a902070dc07e2b1c5ef
Johan Fredrik Wilvang <johan@wilvang.com>
2025-02-13 20:06:51 +0100

    Moved all internal packages to the interal folder. Updated the fields in the Country struct. Implemented a function to request information from the REST countries API.

---
+++ internal/service/infoService.go
@@ -0,0 +1,54 @@
+package service
+
+import (
+	"country-rest-api/internal/constants"
+	"country-rest-api/internal/models"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// RequestInfoService sends an HTTP GET request to the REST Countries API to retrieve information
+// about a country specified by the 'param' parameter. It returns a Country struct with the decoded
+// data or an error if the request or decoding fails.
+func RequestInfoService(param string, limit string) (models.Country, error) {
+	filter := "?fields=name,continents,population,languages,borders,flags,capital"
+	url := constants.RESTCountriesAPI + "alpha/" + param + filter
+
+	r, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return models.Country{}, fmt.Errorf("error in creating request: %v", err)
+	}
+
+	r.Header.Add("content-type", "application/json")
+
+	// Instantiate the client
+	client := &http.Client{}
+	defer client.CloseIdleConnections()
+
+	// Issue request
+	res, err := client.Do(r)
+	if err != nil {
+		return models.Country{}, fmt.Errorf("error in response: %v", err)
+	}
+	defer res.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return models.Country{}, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	// Initialize a Country struct to hold the decoded data
+	country := models.Country{}
+
+	// Decode the JSON data into the Country struct
+	err = json.Unmarshal(body, &country)
+	if err != nil {
+		// If an error occurs during decoding, return a bad request status with the error message
+		return models.Country{}, fmt.Errorf("error during decoding: %v", err)
+	}
+
+	return country, nil
+}
