f7a89294c8190b4703c65c3ebb8dc20788c4615b
Johan Fredrik Wilvang <johan@wilvang.com>
2025-02-15 14:37:00 +0100

    Implemented the requestCities function to retrive information about cities from the CountriesNowAPI.

--- internal/service/infoService.go
+++ internal/service/infoService.go
@@ -1,12 +1,15 @@
 package service
 
 import (
+	"bytes"
 	"country-rest-api/internal/constants"
 	"country-rest-api/internal/models"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 // RequestInfoService sends an HTTP GET request to the REST Countries API to retrieve information
@@ -58,9 +61,13 @@ func RequestInfoService(param string, limit string) (models.Country, error) {
 	// Updates the struct with the nested fields
 	country.Country, country.Flag = extractNestedFields(body)
 
+	cities, err := requestCities(&country, limit)
+	country.Cities = cities
+
 	return country, nil
 }
 
+// extractNestedFields extracts the nested fields 'name' and 'flag' from the JSON response body.
 func extractNestedFields(body []byte) (string, string) {
 
 	// Unmarshal JSON into a map
@@ -74,4 +81,73 @@ func extractNestedFields(body []byte) (string, string) {
 	flag := result["flags"].(map[string]interface{})["png"].(string)
 
 	return name, flag
+}
+
+// requestCities sends an HTTP POST request to the CountriesNow API to retrieve a list of cities
+// for the specified country. It returns a slice of city names or an error if the request or decoding fails.
+func requestCities(country *models.Country, limit string) ([]string, error) {
+
+	url := constants.CountriesNowAPI + "countries/cities"
+
+	cityResponse := models.CityResponse{
+		Error: true,
+		Msg:   "",
+		Data:  make([]string, 0),
+	}
+
+	// Create instance of content
+	requestBody := models.CityRequest{Name: country.Country}
+
+	// Marshal the request data into JSON
+	jsonData, err := json.Marshal(requestBody)
+	if err != nil {
+		log.Printf("error during decoding: %v", err)
+		return cityResponse.Data, fmt.Errorf("error marshalling request data: %v", err)
+	}
+
+	r, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Printf("error during decoding: %v", err)
+		return cityResponse.Data, fmt.Errorf("error in creating request: %v", err)
+	}
+	r.Header.Set("Content-Type", "application/json")
+
+	// Instantiate the client
+	client := &http.Client{}
+	defer client.CloseIdleConnections()
+
+	// Issue request
+	res, err := client.Do(r)
+	if err != nil {
+		log.Printf("error during decoding: %v", err)
+		return cityResponse.Data, fmt.Errorf("error in response: %v", err)
+	}
+	defer res.Body.Close()
+
+	// Check if the response status is not OK
+	if res.StatusCode != http.StatusOK {
+		log.Printf("error during decoding: %v", err)
+		return cityResponse.Data, fmt.Errorf("error: received status code %d", res.StatusCode)
+	}
+
+	// Read the response body
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("error during decoding: %v", err)
+		return cityResponse.Data, fmt.Errorf("error reading response body: %v", err)
+	}
+
+	// Decodes the json response
+	if err := json.Unmarshal(body, &cityResponse); err != nil {
+		log.Printf("error during decoding: HERE %v", err)
+		return cityResponse.Data, fmt.Errorf("error during decoding: %v", err)
+	}
+
+	// Slices the list of cities to the limit if provided
+	if lim, err := strconv.Atoi(limit); err == nil && lim >= 0 {
+		return cityResponse.Data[:lim], nil
+	}
+
+	// returns the complete list of cities
+	return cityResponse.Data, nil
 }
